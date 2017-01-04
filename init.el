077f1ea3d3e52e408d75f84728754f638291e918
etibger <tibi.gerlai@gmail.com>
2017-01-04 16:49:18 +0100

    Update init.el

--- init.el
+++ init.el
@@ -20,6 +20,13 @@
   '(better-defaults
 ;;    flycheck
 ;;    origami
+;;    magit
+;;    elpy
+;;    counsel
+;;    hydra
+;;    ivy-hydra
+;;    ivy-rich
+;;    auto-complet
     material-theme))
 
 (mapc #'(lambda (package)
@@ -32,8 +39,22 @@
 ;; hide the startup message
 (setq inhibit-startup-message t)
 
+(when (require 'elpy nil t)
+  (elpy-enable))
+
+(ac-config-default)
+
+(defadvice auto-complete-mode (around disable-auto-complete-for-python)
+  (unless (eq major-mode 'python-mode) ad-do-it))
+
+(ad-activate 'auto-complete-mode)
+
 ;; enable flychek mode globally
 (global-flycheck-mode)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 ;; enable origami mode by default
 (global-origami-mode t)
@@ -53,11 +74,41 @@
 ;; remove trailing whitspace before save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; enable origami mode by default
-(global-origami-mode t)
-
 ;; enable visual line mode
 (global-visual-line-mode t)
+
+;; enable magits for files
+(global-magit-file-mode)
+
+;; enable ido mode by default
+;;(ido-mode t)
+
+;;
+(ivy-mode t)
+
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(global-set-key (kbd "<f5>") 'kill-this-buffer)
+
+(require 'ivy-rich)
+(ivy-set-display-transformer 'ivy-switch-buffer 'ivy-rich-switch-buffer-transformer)
+
+(setq ivy-virtual-abbreviate 'full
+      ivy-rich-switch-buffer-align-virtual-buffer t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
